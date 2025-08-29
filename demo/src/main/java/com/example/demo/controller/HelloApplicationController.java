e96b419c28fb6f2d95816523a3162f39631ab3b3
shu-kita <shusei3316@yahoo.co.jp>
2025-08-30 00:13:57 +0900

    hello app

---
+++ demo/src/main/java/com/example/demo/controller/HelloApplicationController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloApplicationController {
+
+	@RequestMapping("/")
+	public String test() {
+		return "Hello World";
+	}
+}
+
